f7a4a262d96215c7c7c10323d3d6e01c7c99a714
Skalik2 <106670239+Skalik2@users.noreply.github.com>
2022-12-27 16:44:56 +0100

    Repaired navbar on mobile devices

---
+++ src/components/LogoPocket.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function LogoPocket() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="76.346"
+      height="10mm"
+      version="1.1"
+      viewBox="0 0 20.2 10"
+    >
+      <g
+        fillRule="evenodd"
+        stroke="none"
+        strokeDasharray="none"
+        strokeMiterlimit="4"
+        strokeOpacity="1"
+        paintOrder="normal"
+      >
+        <rect
+          width="19.945"
+          height="9.766"
+          x="0.186"
+          y="0.109"
+          fill="#fff"
+          strokeWidth="0.2"
+          rx="3.044"
+          ry="3.372"
+        ></rect>
+        <path
+          fill="#000"
+          fillOpacity="1"
+          strokeWidth="1.002"
+          d="M291.02 304.286l-10.675 6.163-10.675 6.163V291.96l10.675 6.163z"
+          transform="matrix(.18313 0 0 .18313 -41.227 -50.731)"
+        ></path>
+      </g>
+    </svg>
+  );
+}
+
+export default LogoPocket;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { Stack } from "@mui/material"
 import { Link } from 'react-router-dom'
-import { SearchBar, Logo2 } from '../components/index'
+import { SearchBar, Logo2, LogoPocket } from '../components/index'
 
-const navbar = () =>(
+const Navbar = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.onresize = function() {
+      setScreenWidth(window.innerWidth);
+  }
+  })
+
+  return(
     <Stack direction='row' allignItems="center" p={2} sx={{position:'sticky',background:'#000',top:'0',justifyContent:'space-between'}}>
       <Link to="/" style={{display:'flex',alignItems:'center'}}>
-          <Logo2 />
+        {screenWidth < 380 ? '' : (screenWidth > 820 ? <Logo2 /> : <LogoPocket />)}
       </Link>
       <SearchBar />
     </Stack>
 )
+};
 
 
-export default navbar
+export default Navbar
